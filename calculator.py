637acc71e663449da9fc8f8d20872817e86fbb89
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-05-18 11:54:48 +0400

    added sum functionality

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
 num1 = int(input("Enter a number: "))
 num2= int(input("Enter a number: "))
 operator= input("enter + - * / ** fac mod ... ")
+result = 0
+
+if operator == '+':
+    result = num1 + num2
+    print(f"result = {result}")
