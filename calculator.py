45af9c31000d54c01a0c3c2d378ab440675637ef
unknown <shihab.ahmed.aljassasi@gmail.com>
2025-05-18 11:57:59 +0400

    add - calculation

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
 num1 = int(input("Enter a number: "))
 num2= int(input("Enter a number: "))
 operator= input("enter + - * / ** fac mod ... ")
+
+if operator == "-":
+    resulte = num1 - num2
+
+    print(resulte)
