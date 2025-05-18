d9e05600798a2153bae3490be6b953c6a607cc59
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-05-18 12:14:54 +0400

    added division functionality

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
 num1 = int(input("Enter a number: "))
 num2= int(input("Enter a number: "))
 operator= input("enter + - * / ** fac mod ... ")
-
-if operator == "-":
-    resulte = num1 - num2
-
-    print(resulte)
 result = 0
 
-if operator == '+':
+if operator == "-":
+    result = num1 - num2
+    print(result)
+    
+elif operator == '+':
     result = num1 + num2
+    print(f"result = {result}")
+
+elif operator == '/':
+    result = num1 / num2
     print(f"result = {result}")
