238b080a6e45fba47b10a20c873068e7c66cbc46
unknown <shihab.ahmed.aljassasi@gmail.com>
2025-05-18 12:14:45 +0400

    adding *

--- calculator.py
+++ calculator.py
@@ -3,11 +3,15 @@ num2= int(input("Enter a number: "))
 operator= input("enter + - * / ** fac mod ... ")
 
 if operator == "-":
-    resulte = num1 - num2
-
-    print(resulte)
+    result = num1 - num2
+    print(f"result = {result}")
 result = 0
 
 if operator == '+':
     result = num1 + num2
     print(f"result = {result}")
+
+elif operator =="*":
+    result = num1 * num2
+    print(f"result = {result}")
+    
